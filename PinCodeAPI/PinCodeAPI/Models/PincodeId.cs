e06db8881c2fc476ab8c00fd89a9db76880f7a33
Suyash Baoney <suyashbaoney58@gmail.com>
2024-11-07 23:36:42 +0530

    Initial Commit
    API created for limited functionality using ASP.NET MVC Core framwork and C#

---
+++ PinCodeAPI/PinCodeAPI/Models/PincodeId.cs
@@ -0,0 +1,10 @@
+﻿namespace PinCodeAPI.Models
+{
+    public class PincodeId
+    {
+        public string OfficeName { get; set; }
+        public int Pincode { get; set; }
+        public string District { get; set; }
+        public string DivisionName { get; set; }
+    }
+}
